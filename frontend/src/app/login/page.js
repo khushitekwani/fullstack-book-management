bf44a832b67f9c4513b1e89747aa2720cf796f4d
khushitekwani <97432551+khushitekwani@users.noreply.github.com>
2025-06-04 12:38:50 +0530

    Update page.js

--- frontend/src/app/login/page.js
+++ frontend/src/app/login/page.js
@@ -294,7 +294,7 @@ const Login = () => {
           {/* Sign Up Link */}
           <div className="mt-6 text-center">
             <p className="text-sm text-gray-600">
-              Don't have an account?{' '}
+              Don't have an account?
               <Link href="/register" className="font-medium text-blue-600 hover:text-blue-500">
                 Sign up now
               </Link>
