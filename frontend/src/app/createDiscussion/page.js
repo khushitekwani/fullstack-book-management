15278212b3ff322f02c58e9b7ed2eecbd793539a
khushitekwani <97432551+khushitekwani@users.noreply.github.com>
2025-06-04 12:48:59 +0530

    Update page.js

--- frontend/src/app/createDiscussion/page.js
+++ frontend/src/app/createDiscussion/page.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from "react";
 import { useFormik } from "formik";
 import * as Yup from "yup";
-import { ThumbsUp, MessageSquare } from "lucide-react";
+import { ThumbsUp, MessageSquare, Heart } from "lucide-react";
 import {
   getAllBooks,
   getDiscussions,
